82a405cfe57f263b217f6992f4d382fe1ba7317a
SENSlE <kevinwebsterr2016@gmail.com>
2023-08-23 21:59:41 +0800

    xzczxc

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,64 @@
+"use client"
+import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import {notFound} from 'next/navigation'
+import useSWR from 'swr'
+
+function getData(id) {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+  const { data, error, isLoading } = useSWR(
+    `/api/posts${id}`, fetcher)
+
+
+
+  if (error) {
+   return notFound()
+  }
+ 
+  return data;
+}
+ 
+
+// export async function generateMetadata({ params }) {
+//   const post = await getData(params.id)
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   }
+// }
+
+const  BlogPost = ({params}) => {
+  const data = getData(params.id)
+
+ 
+  return (
+    <div className={styles.container}>
+      <Link className={styles.link} href="/blog">Back</Link>
+      <div className={styles.topContainer}>
+        <div className={styles.content}>
+          <h1>{data.title}</h1>
+          <h2>{data.desc}</h2>
+          <div className={styles.author}>
+            <div className={styles.imgContainer}>
+              <Image src={data.img} fill={true} alt="" />
+            </div>
+              <p>{data.username}</p>
+          </div>
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image src={data.img} fill={true} alt="" />
+        </div>
+      </div>
+
+      <div className={styles.bottomContainer}>
+        <p>{data.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPost
