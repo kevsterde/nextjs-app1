5d42e917e42897a086f814503a7df3991fc55c5b
SENSlE <kevinwebsterr2016@gmail.com>
2023-08-23 22:40:46 +0800

    xcxzc

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -6,30 +6,31 @@ import Link from 'next/link'
 import {notFound} from 'next/navigation'
 
 
-async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: "no-store",
-  });
+// async function getData(id) {
+//   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    return notFound()
-  }
+//   if (!res.ok) {
+//     return notFound()
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 
-export async function generateMetadata({ params }) {
+// export async function generateMetadata({ params }) {
 
-  const post = await getData(params.id)
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
+//   const post = await getData(params.id)
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   };
+// }
 const  BlogPost = async  ({params}) => {
-  const data = await getData(params.id)
-  
+  // const data = await getData(params.id)
+
+  const data = {}
   return ( 
 
     <div className={styles.container}>
