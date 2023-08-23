7cc47e678d5b82469661783c27586ed8fc88b5ef
SENSlE <kevinwebsterr2016@gmail.com>
2023-08-23 20:32:07 +0800

    edit

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -6,10 +6,9 @@ import {notFound} from 'next/navigation'
 
 
 async function getData(id) {
-  const res = await fetch(`https://beautiful-bombolone-944c3e.netlify.app/api/posts/64e4d64af7b0d27c664c992c`,{
+  const res = await fetch(`http://localhost:3000/api/posts/64e4d64af7b0d27c664c992c`,{
     cache: 'no-store'
   });
-
 
   if (!res.ok) {
    return notFound()
@@ -31,29 +30,9 @@ const  BlogPost = async ({params}) => {
   const data = await getData(params.id)
   return (
     <div className={styles.container}>
-      <Link className={styles.link} href="/blog">Back</Link>
-      <div className={styles.topContainer}>
-        <div className={styles.content}>
-          <h1>{data.title}</h1>
-          <h2>{data.desc}</h2>
-          <div className={styles.author}>
-            <div className={styles.imgContainer}>
-              <Image src={data.img} fill={true} alt="" />
-            </div>
-              <p>{data.username}</p>
-          </div>
-        </div>
-
-        <div className={styles.imgContainer}>
-          <Image src={data.img} fill={true} alt="" />
-        </div>
-      </div>
-
-      <div className={styles.bottomContainer}>
-        <p>{data.content}</p>
-      </div>
+      sample
     </div>
   )
 }
 
-export default BlogPost
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getData() {
-  const res = await fetch('https://beautiful-bombolone-944c3e.netlify.app/api/posts',{
+  const res = await fetch('http://localhost:3000/api/posts',{
     cache: 'no-store'
   });
  
